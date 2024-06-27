008cd84d6aae0dcaa16645c920737fdb9cee98d8
Miroljub Grujin <miroljub.grujin@bridgemaker.com>
2024-06-27 13:44:22 +0200

    fix: build fix

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -10,7 +10,7 @@ module.exports = ({ env }) => ({
       user: config.user,
       password: config.password,
       ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
       },
     },
     options: {
--- config/server.js
+++ config/server.js
@@ -7,4 +7,9 @@ module.exports = ({ env }) => ({
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'),
+    },
+  },
 });
